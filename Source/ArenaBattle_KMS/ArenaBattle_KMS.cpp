d29d085c500b66c03d0db001ca855b373b7d90a0
DESKTOP-LKGRCOR\tpzjs <akd3850@gmail.com>
2025-07-19 14:36:04 +0900

    [AB] 기본 프로젝트 셋팅

---
+++ Source/ArenaBattle_KMS/ArenaBattle_KMS.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaBattle_KMS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle_KMS, "ArenaBattle_KMS" );
+ 
---
+++ Source/ArenaBattle_KMS/ArenaBattle_KMSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArenaBattle_KMSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArenaBattle_KMSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArenaBattle_KMSGameMode();
+};
+
+
+
